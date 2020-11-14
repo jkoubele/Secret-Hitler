37c1eaf8d35067fd4760d057f34143f6aebffde6
Jakub Koubele <jakub.koubele@blindspot.ai>
2020-11-14 18:50:20 +0100

    Selecting player name from dropdown menu.

--- static/login.js
+++ static/login.js
@@ -1,6 +1,27 @@
 function register(){
-    var name = document.getElementById("playername").value
+    var name = document.getElementById("select").value    
     console.log("Name: "+name)    
     sessionStorage.setItem('NAME', name);            
     
 }
+
+function update() {
+    var request_public = new XMLHttpRequest();
+    request_public.open('GET', "/game");      
+    request_public.onload = function () {          
+        var ret = JSON.parse(request_public.responseText);   
+        players = ret['players']
+        var s = ""
+        for(var i=0; i<players.length; i++){
+            console.log(players[i])
+            s += "<option>"+players[i]+"</option>"
+        }
+        document.getElementById("select").innerHTML = s
+             
+         
+    }    
+    
+    request_public.send();     
+}
+
+update()
